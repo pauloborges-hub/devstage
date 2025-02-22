44cc8fffd123c7d246f0f95524cd260ef4e784f4
Paulo Borges <victorborges296@hotmail.com>
2025-02-22 12:01:43 -0300

    biome added to the project

--- src/routes/subscribe-to-event-route.ts
+++ src/routes/subscribe-to-event-route.ts
@@ -1,30 +1,34 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { z } from 'zod'
-import { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 
-export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) => {
-   // Define a POST route for '/subscriptions' with request and response validation
-   app.post('/subscriptions', {
+export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
+  // Define a POST route for '/subscriptions' with request and response validation
+  app.post(
+    '/subscriptions',
+    {
       schema: {
-         summary: 'Subscribes the user to the event',
-         tags: ['subscription'],
-         body: z.object({
+        summary: 'Subscribes the user to the event',
+        tags: ['subscription'],
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+        }),
+        response: {
+          201: z.object({
             name: z.string(),
-            email: z.string().email()
-         }),
-         response: {
-            201: z.object({
-               name: z.string(),
-               email: z.string()
-            })
-         }
-      }
-   }, async (request, response) => {
+            email: z.string(),
+          }),
+        },
+      },
+    },
+    async (request, response) => {
       const { name, email } = request.body
 
       // Send a 201 (created) response with the provided name and email
       return response.status(201).send({
-         name,
-         email
+        name,
+        email,
       })
-   })
+    }
+  )
 }
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
-import { fastify } from 'fastify'
 import { fastifyCors } from '@fastify/cors'
-import {
-   validatorCompiler,
-   serializerCompiler,
-   ZodTypeProvider,
-   jsonSchemaTransform
-} from 'fastify-type-provider-zod'
 import { fastifySwagger } from '@fastify/swagger'
 import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import {
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { env } from './env'
 import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 
 // Initialize Fastify app with Zod type provider
@@ -19,27 +20,27 @@ app.setSerializerCompiler(serializerCompiler)
 
 // Enable CORS for request from the specified origin
 app.register(fastifyCors, {
-   origin: 'http://localhost:3000'
+  origin: 'http://localhost:3000',
 })
 
 // Configure Swagger for API documentation
 app.register(fastifySwagger, {
-   openapi: {
-      info: {
-         title: 'Dev Stage',
-         version: '0.0.1'
-      }
-   },
-   transform: jsonSchemaTransform
+  openapi: {
+    info: {
+      title: 'Dev Stage',
+      version: '0.0.1',
+    },
+  },
+  transform: jsonSchemaTransform,
 })
 
 app.register(fastifySwaggerUi, {
-   routePrefix: '/docs'
+  routePrefix: '/docs',
 })
 
 app.register(subscribeToEventRoute)
 
 // Start the server on port 3333 and log a confirmation message
-app.listen({ port: 3333 }).then(() => {
-   console.log('HTTP server running')
+app.listen({ port: env.PORT }).then(() => {
+  console.log('HTTP server running')
 })
